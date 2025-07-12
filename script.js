6a3eddd257709476be9a0cef0b2c700ae53d748f
Cassandra Caluag <cassandralinneacaluag@gmail.com>
2025-07-12 00:35:51 +0000

    push changes

---
+++ script.js
@@ -0,0 +1,28 @@
+// typing effect! 
+const lines = document.querySelectorAll('.type-effect'); // get all elements with the type-effect class
+console.log(lines)
+let line = 0;
+function typeText(elmnt, text, i = 0) {
+  if (i < text.length) {
+    elmnt.textContent += text[i]; // add a letter
+    setTimeout(() => typeText(elmnt, text, i + 1), 40); 
+  } 
+}
+// prepare
+function startTyping(elmnt) {
+  const text = elmnt.dataset.text; 
+  elmnt.textContent = ''; // clear span
+  typeText(elmnt, text);
+}
+window.onload = () => startTyping(lines[line]);
+
+// using typed.js for identity statements - repetitive typing loop because it's cool
+setTimeout(() => {
+  var typed = new Typed("#identity-statements", {
+    strings: ["ENGINEERING STUDENT.", "SOFTWARE DEVELOPER.", "RESILIENT PROBLEM-SOLVER.", "AMBITIOUS IDEALIST.", "WELCOME TO MY PORTFOLIO!"],
+    typeSpeed: 70,
+    backSpeed: 40,
+    backDelay: 1200,
+    loop: true
+  });
+}, 2000); // waits 3 seconds before typing starts
